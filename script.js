4fe201b2a8bf0c3c8718eaf6852763dd62c828ba
wedho <wellingtond99@gmail.com>
2023-06-05 13:43:42 +0200

    initial commit for maffy

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".social-icons i").on("click", function() {
+        alert("Oops! The site is still being developed.");
+    });
+
+    $(".social-icons i").on("select", function() {
+        alert("Oops! The site is still being developed.");
+    });
+});
